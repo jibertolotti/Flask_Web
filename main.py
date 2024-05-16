a9e37192f98f85a848c63eb688f0f53ee73634ba
jibertolotti <135075918+jibertolotti@users.noreply.github.com>
2024-05-16 18:14:14 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return '<h1>Bienvenido!</h1>', "<h1>En esta página hablaremos sobre Asistente, un bot de Discord</h1>"
-    
+def beggining():
+    return "<h1>Bienvenido!</h1>"\
+           "<h1>En esta página hablaremos sobre Asistente, un bot de Discord</h1>"
 
-#@app.route("/info")
+@app.route("/info")
+def description():
+    return "<h1> El bot posee muchas funciones, entre ellas traducir texto y generar códigos QR </h1>"
+    
+@app.route("/access")
+def access():
+    return "<h1>¡Accede al bot ya!</h1>" \
+           "<a href='https://discord.com/oauth2/authorize?client_id=1207304722395168788&permissions=8&scope=bot'>Click aquí para acceder</a>"
+
 
 app.run(debug=True)
